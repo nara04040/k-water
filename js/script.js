37249ff0cec737bd101e1e14ccf6672bd7e6f0b8
Dev.Jin <nara04040@naver.com>
2022-07-06 09:13:24 +0900

    add new pic and css

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
-$(document).ready(function () {
-
-});
+$(document).ready(function () {});
 window.onload = function () {
-    // 랜덤 캐릭터(0~2)
-    let rNum = Math.trunc(Math.random() * 3);
+    // 랜덤 천사 기능(0~2)
+    let rNum = Math.floor(Math.random() * 3);
     let rClass = 'about-box-char-' + rNum;
     let rTag = $('.about-box-sns');
     rTag.addClass(rClass);
@@ -12,11 +10,12 @@ window.onload = function () {
     let header = $('.header');
     let gnb = $('.gnb');
     let gnbH = gnb.height();
+
     gnb.mouseenter(function () {
         header.css('height', gnbH);
     });
-    header.mouseleave(function () {
-        header.css('height', '70')
+    gnb.mouseleave(function () {
+        header.css('height', 70);
     });
 
     // 비주얼 슬라이드
@@ -28,6 +27,7 @@ window.onload = function () {
             nextEl: '.sw-visual-next'
         }
     });
+
     // about 슬라이드
     let swAbout = new Swiper('.sw-about', {
         loop: true,
@@ -40,27 +40,29 @@ window.onload = function () {
             el: '.sw-about-pg',
             type: 'fraction'
         },
-
         navigation: {
-            prevEl: '.sw-about-prev',
-            nextEl: '.sw-about-next'
+            nextEl: '.sw-about-next',
+            prevEl: '.sw-about-prev'
         },
         allowTouchMove: false
     });
+
     let swAboutBt = $('.sw-about-pause');
     swAboutBt.click(function () {
-        //현재 클래스 상태 확인
+        // 현재 class 상태 체크
         let temp = $(this).hasClass('sw-about-play');
         if (temp == true) {
+            // 슬라이드 자동 실행
             swAbout.autoplay.start();
+            // 아이콘을 pause 버튼으로 바꾼다.
+            // 사용자는 멈추기 위해서 클릭을 하도록 안내한다.
             $(this).removeClass('sw-about-play');
         } else {
-            // 슬라이드멈춤
+            // 슬라이드 멈춤
             swAbout.autoplay.stop();
-            // 아이콘을 play 버튼으로 바꾼다
-            // 사용자는 멈춘 슬라이드를 play하려고 할 것이다.
+            // 아이콘을 play 버튼으로 바꾼다.
+            // 사용자는 멈춘 슬라이드를 play 하려고 할것이다.
             $(this).addClass('sw-about-play');
-
         }
     });
 
